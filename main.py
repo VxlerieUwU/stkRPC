c4b6305411ed33b02e96d3e38154fddf555b83a1
undevdecatos <30691126+undevdecatos@users.noreply.github.com>
2020-06-19 22:51:34 +0200

    initial commit

---
+++ main.py
@@ -0,0 +1,20 @@
+from pypresence import Presence
+import time
+import tailer
+import sys
+
+client_id = '699335298567634954'  # Fake ID, put your real one here
+RPC = Presence(client_id)  # Initialize the client class
+RPC.connect() # Start the handshake loop
+
+RPC.update(large_image="stk", state="SuperTuxKart", details="Main menu", start=time.time(), large_text="SuperTuxKart RPC script by undevdecatos!")
+
+for line in tailer.follow(open('~/.config/supertuxkart/config-0.10/stdout.log', encoding='UTF8')):
+    if '[info   ] ProtocolManager: A 12GameProtocol protocol has been started.' in line:
+        RPC.update(large_image="stk", small_image="online", state="SuperTuxKart", details="In an online match", start=time.time())
+    elif '[info   ] ProtocolManager: A 12GameProtocol protocol has been terminated.' in line:
+        RPC.update(large_image="stk", small_image="online", state="SuperTuxKart", details="Waiting in an online lobby", start=time.time())
+    elif '[info   ] ProtocolManager: A 11ClientLobby protocol has been terminated.' in line:
+        RPC.update(large_image="stk", state="SuperTuxKart", details="Main menu", start=time.time())
+    elif '[info   ] ProtocolManager: A 11ClientLobby protocol has been started.' in line:
+        RPC.update(large_image="stk", small_image="online", state="SuperTuxKart", details="Waiting in an online lobby", start=time.time())
