cbb84328a443f001f66816fca19ac8118175b784
32Linux <30691126+undevdecatos@users.noreply.github.com>
2020-06-19 22:55:33 +0200

    Update main.py

--- main.py
+++ main.py
@@ -9,6 +9,8 @@ RPC.connect() # Start the handshake loop
 
 RPC.update(large_image="stk", state="SuperTuxKart", details="Main menu", start=time.time(), large_text="SuperTuxKart RPC script by undevdecatos!")
 
+# TODO: detect the log path according to os
+
 for line in tailer.follow(open('~/.config/supertuxkart/config-0.10/stdout.log', encoding='UTF8')):
     if '[info   ] ProtocolManager: A 12GameProtocol protocol has been started.' in line:
         RPC.update(large_image="stk", small_image="online", state="SuperTuxKart", details="In an online match", start=time.time())
